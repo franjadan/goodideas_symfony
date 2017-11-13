949070166a2c6cfd9eff5498a6997a67b05a81f9
Luis Ramón López <lrlopez@gmail.com>
2017-11-13 22:10:02 +0100

    Extraer listado de ejemplo de ideas a una función independiente

--- src/AppBundle/Controller/IdeaController.php
+++ src/AppBundle/Controller/IdeaController.php
@@ -12,14 +12,19 @@ class IdeaController extends Controller
      */
     public function listarAction()
     {
-        $ideas = [
-            ['titulo' => 'Doblar el número de horas de HLC', 'descripcion' => 'Estaría bien que HLC tuviera más horas y así poder hacer más ejercicios', 'autor' => 'Chuck Norris', 'fecha_propuesta' => new \DateTime('2017-11-11')],
-            ['titulo' => 'Irnos de tapas', 'descripcion' => '¿A quién no le apetece que nos echemos unas tapas un día de estos al acabar las clases?', 'autor' => 'Juan Nadie', 'fecha_propuesta' => new \DateTime('2017-11-12')],
-            ['titulo' => 'Aprobado general', 'descripcion' => 'Anda, apruébanos ya a todos y así no tenemos que venir más a clase', 'autor' => 'Oso Yogui', 'fecha_propuesta' => new \DateTime('2017-11-13')]
-        ];
+        $ideas = $this->getIdeas();
 
         return $this->render('idea/listar.html.twig', [
             'ideas' => $ideas
         ]);
+    }
+
+    private function getIdeas()
+    {
+        return [
+            ['titulo' => 'Doblar el número de horas de HLC', 'descripcion' => 'Estaría bien que HLC tuviera más horas y así poder hacer más ejercicios', 'autor' => 'Chuck Norris', 'fecha_propuesta' => new \DateTime('2017-11-11')],
+            ['titulo' => 'Irnos de tapas', 'descripcion' => '¿A quién no le apetece que nos echemos unas tapas un día de estos al acabar las clases?', 'autor' => 'Juan Nadie', 'fecha_propuesta' => new \DateTime('2017-11-12')],
+            ['titulo' => 'Aprobado general', 'descripcion' => 'Anda, apruébanos ya a todos y así no tenemos que venir más a clase', 'autor' => 'Oso Yogui', 'fecha_propuesta' => new \DateTime('2017-11-13')]
+        ];
     }
 }
