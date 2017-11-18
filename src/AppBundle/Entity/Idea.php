d2272d67edd63e781e7aa55c420ec4cae288e395
Luis Ramón López <lrlopez@gmail.com>
2017-11-18 13:14:02 +0100

    Añadidas anotaciones a la entidad Idea

--- src/AppBundle/Entity/Idea.php
+++ src/AppBundle/Entity/Idea.php
@@ -2,15 +2,38 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="idea")
+ */
 class Idea
 {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
     private $id;
 
+    /**
+     * @ORM\Column(type="string")
+     */
     private $titulo;
 
+    /**
+     * @ORM\Column(type="string")
+     */
     private $descripcion;
 
+    /**
+     * @ORM\Column(type="string")
+     */
     private $autor;
 
+    /**
+     * @ORM\Column(type="date")
+     */
     private $fechaPropuesta;
 }
