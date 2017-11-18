004ce3cf6c4f008915b3168b7364ae6251c46257
Luis Ramón López <lrlopez@gmail.com>
2017-11-18 19:29:37 +0100

    Añadida una nueva ruta para crear una idea desde PHP
    Nota: no se deben crear o modificar registros de esta forma (usando
    el método GET) porque si el usuario recarga la página o el navegador
    visita el enlace para precargar el contenido se podría ejecutar varias
    veces de forma indebida

--- src/AppBundle/Controller/IdeaController.php
+++ src/AppBundle/Controller/IdeaController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Idea;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class IdeaController extends Controller
 {
@@ -18,6 +19,33 @@ class IdeaController extends Controller
         return $this->render('idea/listar.html.twig', [
             'ideas' => $ideas
         ]);
+    }
+
+    /**
+     * @Route("/idea/nueva", name="idea_nueva")
+     */
+    public function nuevaAction()
+    {
+        // obtenemos el EntityManager, que supervisa las entidades
+        $em = $this->getDoctrine()->getManager();
+
+        // creamos una nueva idea simplemente creando una instancia de la entidad y diciéndole al
+        // EntityManager que la supervise
+        $idea = new Idea();
+
+        $em->persist($idea);
+
+        // rellenamos sus datos usando los setters (al ser fluent, se pueden poner en cascada)
+        $idea
+            ->setTitulo('Esta idea nueva mola un montón')
+            ->setDescripcion('Se pueden crear ideas desde el código PHP de forma muy sencilla')
+            ->setAutor('Chuck Norris')
+            ->setFechaPropuesta(new \DateTime());
+
+        // decimos al EntityManager que queremos guardar todos los cambios hechos en las entidades
+        $em->flush();
+
+        return new Response('<html><body>Nueva idea registrada</body>');
     }
 
     /**
