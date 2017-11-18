710c90656c1ea758b8a8d043ec31eb5c49e105b1
Luis Ramón López <lrlopez@gmail.com>
2017-11-18 22:11:33 +0100

    Añadidos getters y setters a Usuario

--- src/AppBundle/Entity/Usuario.php
+++ src/AppBundle/Entity/Usuario.php
@@ -14,46 +14,221 @@ class Usuario
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      * @ORM\Column(type="integer")
+     *
+     * @var integer
      */
     private $id;
 
     /**
      * @ORM\Column(type="string")
+     *
+     * @var string
      */
     private $nombreUsuario;
 
     /**
      * @ORM\Column(type="string")
+     *
+     * @var string
      */
     private $password;
 
     /**
      * @ORM\Column(type="string")
+     *
+     * @var string
      */
     private $nombre;
 
     /**
      * @ORM\Column(type="string")
+     *
+     * @var string
      */
     private $apellidos;
 
     /**
      * @ORM\Column(type="string")
+     *
+     * @var string
      */
     private $correoElectronico;
 
     /**
      * @ORM\Column(type="boolean")
+     *
+     * @var boolean
      */
     private $administrador;
 
     /**
      * @ORM\Column(type="boolean")
+     *
+     * @var boolean
      */
     private $seleccionador;
 
     /**
      * @ORM\Column(type="boolean")
+     *
+     * @var boolean
      */
     private $moderador;
+
+
+
+    /// Getters y setters
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNombreUsuario()
+    {
+        return $this->nombreUsuario;
+    }
+
+    /**
+     * @param string $nombreUsuario
+     * @return Usuario
+     */
+    public function setNombreUsuario($nombreUsuario)
+    {
+        $this->nombreUsuario = $nombreUsuario;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     * @return Usuario
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * @param string $nombre
+     * @return Usuario
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getApellidos()
+    {
+        return $this->apellidos;
+    }
+
+    /**
+     * @param string $apellidos
+     * @return Usuario
+     */
+    public function setApellidos($apellidos)
+    {
+        $this->apellidos = $apellidos;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCorreoElectronico()
+    {
+        return $this->correoElectronico;
+    }
+
+    /**
+     * @param string $correoElectronico
+     * @return Usuario
+     */
+    public function setCorreoElectronico($correoElectronico)
+    {
+        $this->correoElectronico = $correoElectronico;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAdministrador()
+    {
+        return $this->administrador;
+    }
+
+    /**
+     * @param bool $administrador
+     * @return Usuario
+     */
+    public function setAdministrador($administrador)
+    {
+        $this->administrador = $administrador;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isSeleccionador()
+    {
+        return $this->seleccionador;
+    }
+
+    /**
+     * @param bool $seleccionador
+     * @return Usuario
+     */
+    public function setSeleccionador($seleccionador)
+    {
+        $this->seleccionador = $seleccionador;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isModerador()
+    {
+        return $this->moderador;
+    }
+
+    /**
+     * @param bool $moderador
+     * @return Usuario
+     */
+    public function setModerador($moderador)
+    {
+        $this->moderador = $moderador;
+        return $this;
+    }
+
 }
