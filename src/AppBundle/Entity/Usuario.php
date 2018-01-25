22a066399b54b8507b17ff7ffca4663e68c2d30e
Luis Ramón López <lrlopez@gmail.com>
2018-01-25 22:52:13 +0100

    Implementar la interfaz UserInterface en la entidad Usuario
    Esto nos permite preparar la entidad para que Symfony pueda gestionar
    usuarios desde ella

--- src/AppBundle/Entity/Usuario.php
+++ src/AppBundle/Entity/Usuario.php
@@ -5,12 +5,13 @@ namespace AppBundle\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="AppBundle\Repository\UsuarioRepository")
  * @ORM\Table(name="usuario")
  */
-class Usuario
+class Usuario implements UserInterface
 {
     /**
      * @ORM\Id
@@ -364,5 +365,80 @@ class Usuario
     public function getHistorial()
     {
         return $this->historial;
+    }
+
+    /**
+     * Returns the roles granted to the user.
+     *
+     * <code>
+     * public function getRoles()
+     * {
+     *     return array('ROLE_USER');
+     * }
+     * </code>
+     *
+     * Alternatively, the roles might be stored on a ``roles`` property,
+     * and populated in any number of different ways when the user object
+     * is created.
+     *
+     * @return (Role|string)[] The user roles
+     */
+    public function getRoles()
+    {
+        // inicialmente todos los usuarios tienen el rol ROLE_USER
+        $roles = ['ROLE_USER'];
+
+        // si es administrador, añadir el rol ROLE_ADMIN
+        if ($this->isAdministrador()) {
+            $roles[] = 'ROLE_ADMIN';
+        }
+
+        // si es moderador, añadir el rol ROLE_MODERADOR
+        if ($this->isModerador()) {
+            $roles[] = 'ROLE_MODERADOR';
+        }
+
+        // si es seleccionador, añadir el rol ROLE_SELECCIONADOR
+        if ($this->isSeleccionador()) {
+            $roles[] = 'ROLE_SELECCIONADOR';
+        }
+
+        return $roles;
+    }
+
+    /** getPassword() estaba ya definido, así que no hace falta tocarlo */
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+        // no usamos sal para codificar las contraseñas
+        return null;
+    }
+
+    /**
+     * Returns the username used to authenticate the user.
+     *
+     * @return string The username
+     */
+    public function getUsername()
+    {
+        return $this->getNombreUsuario();
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+        // no hacer nada
     }
 }
